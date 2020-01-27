e728692e22af0383039cdfa12c2af7193b9a4448
davide.patti <davide.patti@autotrader.co.uk>
2020-01-27 18:55:52 +0000

    added TextToSpeech example

--- app/src/main/java/com/example/android_speech/MainActivity.java
+++ app/src/main/java/com/example/android_speech/MainActivity.java
@@ -4,22 +4,65 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.android_speech.authentication.EmailPasswordActivity;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+    private TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeTextToSpeech();
     }
+
+    public void convertTextToSpeech(View view){
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        Log.i("TTS", "button clicked: " + message);
+
+        int speechStatus = textToSpeech.speak(message, TextToSpeech.QUEUE_FLUSH, null, null);
+
+        if (speechStatus == TextToSpeech.ERROR) {
+            Log.e("TTS", "Error in converting Text to Speech!");
+        }
+    }
+
 
     public void firebaseAuthentication(View view){
         Intent intent = new Intent(this, EmailPasswordActivity.class);
         startActivity(intent);
     }
 
+
+    private void initializeTextToSpeech(){
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int ttsLang = textToSpeech.setLanguage(Locale.ENGLISH);
+
+                    if (ttsLang == TextToSpeech.LANG_MISSING_DATA
+                            || ttsLang == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "The Language is not supported!");
+                    } else {
+                        Log.i("TTS", "Language Supported.");
+                    }
+                    Log.i("TTS", "Initialization success.");
+                } else {
+                    Toast.makeText(getApplicationContext(), "TTS Initialization failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        );
+    }
 
 }
